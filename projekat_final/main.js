b8a39e32eb10f17b1ec2bab785c0bd6dbf58e409
PrijateljMilan <77919393+PrijateljMilan@users.noreply.github.com>
2021-08-23 12:49:53 +0200

    Add files via upload

--- projekat_final/main.js
+++ projekat_final/main.js
@@ -27,34 +27,3 @@ fetch("https://localhost:5001/Prodavnica/PreuzmiProdavnice").then( p => {
         });
     });
 
-        
-/*let pr = new Prodavnica();   
-let kat=new Kategorija();
-let kat2=new Kategorija();
-let kat3=new Kategorija();
-let kat4=new Kategorija();
-pr.dodajKategoriju(kat);
-pr.dodajKategoriju(kat2);
-pr.dodajKategoriju(kat3);
-pr.dodajKategoriju(kat4);
-
-
-let proizvod = new Proizvod(3, "Mung pasulj", 465, 50, "500g", "slike/zitarice/MungoPasulj.jpg");
-kat2.dodajProizvod(proizvod);
-
-proizvod = new Proizvod(4, "Azuki pasulj", 400, 40, "500g", "slike/zitarice/AzukiPasulj.jpg");
-kat2.dodajProizvod(proizvod);
-
-proizvod = new Proizvod(5, "Integralna makarona", 100, 30, "400g", "slike/testenine/Testenine makaroni integralni_300x300.jpg");
-kat4.dodajProizvod(proizvod);
-
-
-
-
-
-pr.crtaj(document.body);
-
-/*<meta charset="UTF-8"/>
-<meta name = "viewport" content = "width=device-width, inital-scale = 1.0">
-<link rel="stylesheet" href="stylesheet.css"/>
-<link rel="shortcut icon" href="#" />*/
